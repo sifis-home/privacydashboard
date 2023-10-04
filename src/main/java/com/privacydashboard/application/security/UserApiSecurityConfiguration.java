ed9f730b8fb749427a823cdfac91f2300fb0cfe5
marco-rasori <marco.rasori@gmail.com>
2023-10-04 14:36:31 +0200

    JWT auth via root / page

--- src/main/java/com/privacydashboard/application/security/UserApiSecurityConfiguration.java
+++ src/main/java/com/privacydashboard/application/security/UserApiSecurityConfiguration.java
@@ -1,20 +1,20 @@
-package com.privacydashboard.application.security;
-
-import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-@Configuration
-@Order(2)
-public class UserApiSecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/userapi/**")
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-}
+//package com.privacydashboard.application.security;
+//
+//import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@Configuration
+//@Order(2)
+//public class UserApiSecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.antMatcher("/userapi/**")
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+//    }
+//}
